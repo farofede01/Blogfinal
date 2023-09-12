7b2692c01b3bacff161f7ad5d17cb7580d1ef284
Federico <farolafede2@gmail.com>
2023-09-12 14:55:45 -0300

    Decimo primer commit
    Error al hacer migraciones

--- AppRegistro/models.py
+++ AppRegistro/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Usuario(models.Model):
+class Usuarios (AbstractUser):
     nombre_usuario = models.CharField(max_length=30, unique=True)
     nombre = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    contraseña = models.CharField(max_length=50, null=True)
-    def __str__(self):
-        return f"{self.nombre_usuario}-{self.nombre}-{self.apellidos}-{self.email}"-{self.contraseña}
+
+    def _str_(self):
+        return f"{self.nombre_usuario}-{self.nombre}-{self.apellidos}-{self.email}"
     
--- AppRegistro/views.py
+++ AppRegistro/views.py
@@ -3,29 +3,29 @@ from django.shortcuts import render
 from AppRegistro.models import *
 from django.http import HttpResponse
 from .forms import *
+from .forms import UserRegisterForm
 
 # Create your views here.
-def registro (request):
+def registro(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             info = form.cleaned_data
-            nombre_usuario = info ["nombreusuario"] 
-            nombre = info ["nombre"]
-            apellidos = info ["apellidos"]
-            email = info ["email"]
-            contraseña = info ["contraseña"]
-            usuario = Usuario(nombre_usuario = nombre_usuario, nombre = nombre, apellidos = apellidos ,email = email, contraseña = contraseña)
+            nombre_usuario = info["username"]  
+            nombre = info["nombre"]
+            apellidos = info["apellidos"]
+            email = info["email"]
+            contraseña = info["password1"]  
+            usuario = Usuarios(nombre_usuario=nombre_usuario, nombre=nombre, apellidos=apellidos, email=email, contraseña=contraseña)
             usuario.save()
-            formulario_registro = UserCreationForm()
-            return render (request, "AppRegistro/registro.html", {"mensaje":"Usuario creado"})
+            formulario_registro = UserRegisterForm()
+            return render(request, "AppRegistro/registro.html", {"mensaje": "Usuario creado"})
         else:
-            return render (request, "AppRegistro/registro.html", {"mensaje": "Datos invalidos"})
-    else:   
-        formulario_registro = UserCreationForm()
-    
-    return render(request, "AppRegistro/registro.html", {"formulario": formulario_registro})
+            return render(request, "AppRegistro/registro.html", {"mensaje": "Datos inválidos"})
+    else:
+        formulario_registro = UserRegisterForm()
 
+    return render(request, "AppRegistro/registro.html", {"formulario": formulario_registro})
 
 
             
